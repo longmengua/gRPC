1a48e960fba62a429fffce37f6ab7c13d12b0ba8
waltor <waltor-huang@innotech.me>
2024-05-14 17:08:58 +0800

    update

--- node/service/greeter.ts
+++ node/service/greeter.ts
@@ -1,30 +1,34 @@
-import { credentials } from '@grpc/grpc-js';
+import { credentials, makeClientConstructor } from '@grpc/grpc-js';
 import { hello } from '../pb/hello'
 
 // 建立一個函數用於呼叫 gRPC 服務
-export function callGreeterService(): Promise<string> {
+export async function callGreeterService(): Promise<string | undefined> {
     const grpcServerAddress = "0.0.0.0:8001";
 
-    return new Promise((resolve, reject) => {
+    const res = await new Promise<string | undefined>((resolve, reject) => {
         try {
+            // const client: hello.GreeterClient = makeClientConstructor(hello.UnimplementedGreeterService.definition, hello.GreeterClient.serviceName) as unknown as hello.GreeterClient
             // 建立要傳送的請求訊息
             const request = new hello.HelloRequest();
             request.name = "John";
 
             // 呼叫 gRPC 服務
             const client = new hello.GreeterClient(grpcServerAddress, credentials.createInsecure());
-            client?.sayHello(request, (error: any, response: any) => {
+            client.SayHello(request, (error: any, response: hello.HelloResponse | undefined) => {
                 if (error) {
-                    console.error("Error:", error.message);
-                    reject(error);
-                } else {
-                    // 處理 gRPC 服務器的回應
-                    resolve(response.getMessage());
+                    resolve(undefined);
+                } 
+                
+                if (!response) {
+                    resolve(undefined)
                 }
+
+                resolve(response?.message)
             });
         } catch (error) {
-            console.error("Error:", error);
-            reject(error);
+            resolve(undefined);
         }
     });
+
+    return res
 }
